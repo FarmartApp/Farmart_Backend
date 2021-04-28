ade355ce4b478c18616e11975b44dbc9931c2a36
keerththy <keerththy2012@gmail.com>
2021-04-28 10:33:59 +0530

    hyy

--- src/main/java/com/farm/controllers/UserController.java
+++ src/main/java/com/farm/controllers/UserController.java
@@ -19,10 +19,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-
-
-
-
 @RestController
 @RequestMapping(Constants.BASE_URI)
 public class UserController {
